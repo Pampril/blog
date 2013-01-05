2c380cc617dcd38604fb4009a4ba81df6093aab4
CyrilD <doyencyril@gmail.com>
2013-01-05 20:45:46 +0100

    upload image

--- application/controllers/ImagesController.php
+++ application/controllers/ImagesController.php
@@ -9,30 +9,27 @@ class ImagesController extends Zend_Controller_Action
 	
 	public function ajoutimagesAction()
 	{
-		$image= new Images();    	
-    	$addImage = $image->createRow();
-    	
-		// Création du formulaire et déclaration des paramètres généraux
-		$form = new AjoutImage();
+		$form = new AjoutImage;
 		
-	
-		        
-		//
-		if($this->getRequest()->isPost() && $form->isValid($_POST))
+		if(isset($_POST['ajouter']))
+		{	
+			//récup les données formulaire			
+			$fichier=$form->getValues();
+			
+			$upload = PUBLIC_PATH.'/img/uploads'.$fichier['image'];				
+			
+			$data['destination'] = $upload ;
+			
+			$image = new Images;
+			$ajoutImage = $image->createRow($data);
+			$ajoutImage->save();
+		}
+		else 
 		{
-		    // Retrait des informations depuis les données en post
-		    // et ajout dans le modèle
-// 		    $values = $form->getValues();		    
-// 		    $addImage->setFromArray(array_intersect_key($values, $addImage->toArray()));
-		    
-// 		    var_dump($values);   
-		   
-// 		    $addImage->save();		
-
-			echo $_POST['nomFichier'];
-        }
-        
-       echo $form;
+			
+			echo $form;
+			
+		}
 	}
 
 }
--- application/forms/AjoutImage.php
+++ application/forms/AjoutImage.php
@@ -4,21 +4,26 @@ class AjoutImage extends Zend_Form
 
     public function init()
     {
-        // Champ upload "nomFichier"
-		//$nomFichier = new Zend_Form_Element_File('nomFichier');
-    	//Img
-    	$nomFichier = $this->createElement('file', 'nomFichier');
-    	
-    	
-    	 
-		$nomFichier->setDestination(PUBLIC_PATH. '/img/uploads/');
-		$nomFichier->setLabel('Fichier');
-		$nomFichier->setRequired(true);
-		
-		$this->setMethod('POST');
-		$this->setAction('/images/ajoutimages');
-		$this->setEnctype(Zend_Form::ENCTYPE_MULTIPART);
+       	$image = new Zend_Form_Element_File('image');
+		$image->setLabel('Image')
+				->addValidator('Size', false, '10MB')
+				->addValidator('Extension', false,'jpg,png,gif')
+				->setDecorators(
+						array('File', 'Description', 'Errors',
+							array(array('data'=>'HtmlTag'), array('tag' => 'td')),
+							array('Label', array('tag' => 'th')),
+							array(array('row'=>'HtmlTag'),array('tag'=>'tr'))
+						)
+					)
+				->setAttribs(array(  'id' => 'id_form_image' 
+				, 'enctype' => 'multipart/form-data'
+				))
+				->setValueDisabled(true)
+                ->setDestination(PUBLIC_PATH.'/img/uploads');
 				
-		$this->addElement($nomFichier)->addElement('submit', 'ajouter', array('label' => 'Ajouter'));
+		
+		
+		$this->addElement($image, 'image');	
+		$this->addElement('submit', 'ajouter', array('label' => 'Ajouter'));
     }
 }
